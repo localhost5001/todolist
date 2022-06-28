a7528044d767529ac81433f9df793d84148ad4f7
Leo Kim <e_leokim@yahoo.com>
2022-06-28 12:00:25 -0700

    web: minor changes to layout component

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -3,9 +3,16 @@ import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <>
-    <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">Todos</a>
-    </div>
+    <div className="navbar mx-auto bg-base-300">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">TODOs</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </button>
+      </div>
+  </div>
     <Outlet />
     </>
   )
